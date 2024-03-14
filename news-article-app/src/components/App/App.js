c2ba6cc7bad602b8cb1369529a1eb2e8c9a3ec69
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-03-14 03:39:29 -0500

    Introduce faux articleData and source it through components

--- news-article-app/src/components/App/App.js
+++ news-article-app/src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ArticleList from '../ArticleList/ArticleList';
+import { articleData } from '../../articleData';
 
 function App() {
+ 
   return (
     <div className="App">
-      <ArticleList /> 
+      <ArticleList articles={articleData.articles} /> 
     </div>
   );
 }
